17c4fbab0610cd78a4113ffa2e861a493cb31257
blasted-tiara <enver.podgorcevic@gmail.com>
2025-06-14 09:05:02 +0100

    Add sprites and basic player and tile structs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,80 @@
-// This is where your main game loop code goes
-// The stuff in this block will run ~60x per sec
-turbo::go!({
-    text!("Hello, world!!!");
-});
+const TILE_SIZE: i32 = 16;
+
+#[derive(BorshDeserialize, BorshSerialize, Debug, Clone, PartialEq)]
+struct Player {
+    x: f32,
+    y: f32,
+    speed_x: f32,
+    speed_y: f32,
+    max_gravity: f32,
+    is_falling: bool,
+    is_facing_left: bool,
+    is_landed: bool,
+}
+
+impl Player {
+    fn new(x: f32, y: f32) -> Self {
+        Self {
+            x,
+            y,
+            speed_x: 0.0,
+            speed_y: 0.0,
+            max_gravity: 15.0,
+            is_falling: false,
+            is_facing_left: false,
+            is_landed: false,
+        }
+    }
+
+    fn draw(&self) {
+        sprite!(
+            "kiwi_idle",
+            x = self.x as i32,
+            y = self.y as i32,
+            flip_x = self.is_facing_left,
+        )
+    }
+}
+
+#[derive(BorshSerialize, BorshDeserialize, PartialEq, Debug, Clone)]
+struct Tile {
+    grid_x: usize,
+    grid_y: usize,
+}
+
+impl Tile {
+    fn new(grid_x: usize, grid_y: usize) -> Self {
+        Self { grid_x, grid_y }
+    }
+
+    fn draw(&self) {
+        let x = self.grid_x as i32 * TILE_SIZE;
+        let y = self.grid_y as i32 * TILE_SIZE;
+
+        sprite!("tile", x = x, y = y);
+    }
+}
+
+turbo::init!(
+    struct GameState {
+        player: Player,
+        tiles: Vec<Tile>,
+    } = {
+        let mut tiles = Vec::new();
+        tiles.push(Tile::new(7, 8));
+        GameState {
+            player: Player::new(110., 112.),
+            tiles,
+        }
+    }
+);
+
+turbo::go!(
+    let mut state = GameState::load();
+    clear(0xadd8e6ff);
+    for t in &mut state.tiles{
+        t.draw();
+    }
+    state.player.draw();
+    state.save();
+);
