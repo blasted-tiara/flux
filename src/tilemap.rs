61641b1c9005a99542d544318c1e0728bedce5f0
blasted-tiara <sectioaurea13@gmail.com>
2025-08-12 01:15:35 +0200

    Fix annoying issue with clamp

--- src/tilemap.rs
+++ src/tilemap.rs
@@ -98,9 +98,9 @@ impl TileMap {
     
     pub fn lock_viewport_to_tilemap(self: &Self, position: &Vector2, viewport_dimensions: &Vector2) -> Vector2 {
         let min_x = viewport_dimensions.x / 2.;
-        let max_x = self.width - viewport_dimensions.x / 2.;
+        let max_x = f32::max(min_x, self.width - viewport_dimensions.x / 2.);
         let min_y = viewport_dimensions.y / 2.;
-        let max_y = self.height - viewport_dimensions.y / 2.;
+        let max_y = f32::max(min_y, self.height - viewport_dimensions.y / 2.);
         
         Vector2::new(position.x.clamp(min_x, max_x), position.y.clamp(min_y, max_y))
     } 
