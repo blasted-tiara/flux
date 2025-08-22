6f3b1e7e28be80c3e6393b0fd8491f6d4343b28c
blasted-tiara <sectioaurea13@gmail.com>
2025-08-22 07:36:39 -0400

    Remove old constructors

--- src/flux.rs
+++ src/flux.rs
@@ -16,34 +16,6 @@ pub enum FluxCoreType {
 }
 
 impl FluxCore {
-    pub fn new_radial_core(amplitude: f32, position: Vector2, width: f32, height: f32) -> Self {
-        Self {
-            amplitude,
-            core_type: FluxCoreType::Radial,
-            time_offset: 0.,
-            period_s: 0.,
-            solid: Solid {
-                position,
-                width,
-                height,
-            }
-        }
-    }
-    
-    pub fn new_rotational_core(amplitude: f32, position: Vector2, width: f32, height: f32) -> Self {
-        Self {
-            amplitude,
-            core_type: FluxCoreType::Rotational,
-            time_offset: 0.,
-            period_s: 0.15,
-            solid: Solid {
-                position,
-                width,
-                height,
-            }
-        }
-    }
-    
     pub fn draw(&self) {
         let x = (self.solid.position.x - self.solid.width / 2.) as i32;
         let y = (self.solid.position.y - self.solid.height / 2.) as i32;
